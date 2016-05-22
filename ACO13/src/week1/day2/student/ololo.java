00570f5766907ae02d69f059c02fdced20581626
debasser1911 <debasser1911@gmail.com>
2016-05-22 22:15:43 +0300

    ololo

--- ACO13/src/week1/day2/student/ololo.java
+++ ACO13/src/week1/day2/student/ololo.java
@@ -4,4 +4,7 @@ package week1.day2.student;
  * Created by Andrey on 22.05.2016.
  */
 public class ololo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
