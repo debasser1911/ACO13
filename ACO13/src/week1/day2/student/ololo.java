db64e7a57db33a1abc5e5691972979a414544962
debasser1911 <debasser1911@gmail.com>
2016-05-22 22:22:12 +0300

    second student test

--- ACO13/src/week1/day2/student/ololo.java
+++ ACO13/src/week1/day2/student/ololo.java
@@ -1,10 +1,18 @@
 package week1.day2.student;
 
+import utils.MyDate;
+
 /**
  * Created by Andrey on 22.05.2016.
  */
 public class ololo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Student st6 = new Student("Ivan", new MyDate(1992,3,24), 'm');
+        Student st7 = new Student("Peta", new MyDate(1987,3,24), 'm');
+
+        System.out.println(st6.toString());
+        System.out.println(st7.toString());
+
     }
 }
