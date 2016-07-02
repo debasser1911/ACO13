005790930eeb3ffd55ab785eb4aa45cbc6ef5ee9
debasser <debasser1911@gmai.com>
2016-07-02 15:12:15 +0300

    kkk

--- ACO13/src/homeworktest/MyStringTest.java
+++ ACO13/src/homeworktest/MyStringTest.java
@@ -10,9 +10,6 @@ public class MyStringTest {
         char[] chars = {'a', 'b', 'c', 'd', 'e', 'f','j','f','s'};
         char[] chars2 = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
         char[] chars4 = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
-        char[] chars3 = new char[10];
-        char[] chars6 = new char[10];
-        char[] chars7 = new char[5];
         String str = "abcdefgjklmnopst";
         MyString mystring = new MyString(chars);
         MyString mystring2 = new MyString(chars2);
