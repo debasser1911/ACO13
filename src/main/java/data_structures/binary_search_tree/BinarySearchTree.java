1c6602a98706c3f1063c827b97a4f0072af3bc74
DeBasser <debasser1911@gmail.com>
2016-07-03 18:05:26 +0300

    CW_commit

--- src/main/java/data_structures/binary_search_tree/BinarySearchTree.java
+++ src/main/java/data_structures/binary_search_tree/BinarySearchTree.java
@@ -1,11 +1,11 @@
-package data_structures.binary_search_tree;
+package main.java.data_structures.binary_search_tree;
 
 import java.util.*;
 
 /**
  * Created by debasser on 02.07.16.
  */
-public class BinarySearchTree<E> implements Set<E>, Iterable<E> {
+public class BinarySearchTree<E> extends AbstractSet<E> implements NavigableSet<E>, Set<E>, Iterable<E> {
     private Node<E> root;
     private int size;
     private Comparator<? super E> comparator;
@@ -19,7 +19,7 @@ public class BinarySearchTree<E> implements Set<E>, Iterable<E> {
 
     @Override
     public int size() {
-        return size();
+        return this.size();
     }
 
     @Override
@@ -29,12 +29,134 @@ public class BinarySearchTree<E> implements Set<E>, Iterable<E> {
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        Node<E> search = null;
+
+        if (comparator != null) {
+            //todo search with comparator
+        } else {
+            search = findNode(o);
+
+        }
+        return search != null;
+    }
+
+    private Node<E> findNode(Object o) {
+        if (o == null) throw new NullPointerException();
+        Node<E> iter = root;
+        int compare;
+        @SuppressWarnings("unchecked")
+        Comparable<? super E> comparable = (Comparable<? super E>) o;
+        while (iter != null) {
+            compare = comparable.compareTo(iter.value);
+            if (compare > 0) iter = iter.rightChild;
+            else if (compare < 0) iter = iter.leftChild;
+            else return iter;
+        }
+        return null;
+    }
+
+
+    @Override
+    public E lower(E e) {
+        return null;
+    }
+
+    @Override
+    public E floor(E e) {
+        return null;
+    }
+
+    @Override
+    public E ceiling(E e) {
+        return null;
+    }
+
+    @Override
+    public E higher(E e) {
+        return null;
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new MyBinaryTreeIterator();
+    }
+
+    @Override
+    public NavigableSet<E> descendingSet() {
+        return null;
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> subSet(E fromElement, boolean fromInclusive, E toElement, boolean toInclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> headSet(E toElement, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public NavigableSet<E> tailSet(E fromElement, boolean inclusive) {
+        return null;
+    }
+
+    @Override
+    public Comparator<? super E> comparator() {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> subSet(E fromElement, E toElement) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> headSet(E toElement) {
+        return null;
+    }
+
+    @Override
+    public SortedSet<E> tailSet(E fromElement) {
+        return null;
+    }
+
+    @Override
+    public E first() {
+        if (isEmpty()) throw new NoSuchElementException("size = 0");
+        Node<E> iter = root;
+        return findLeft(iter);
+
+    }
+
+    private E findLeft(Node<E> iter) {
+        return iter.leftChild != null ? findLeft(iter.leftChild) : iter.value;
+    }
+
+    @Override
+    public E last() {
+        if (isEmpty()) throw new NoSuchElementException("size = 0");
+        Node<E> iter = root;
+        return findRight(iter);
+    }
+
+    private E findRight(Node<E> iter) {
+        return iter.rightChild != null ? findRight(iter.rightChild) : iter.value;
     }
 
     @Override
@@ -49,38 +171,130 @@ public class BinarySearchTree<E> implements Set<E>, Iterable<E> {
 
     @Override
     public boolean add(E e) {
-        if(root == null){
+        if (root == null) {
+            canCompare(e, e);
             root = new Node<>(e);
             size++;
             return true;
 
         }
 
-        if(comparator != null){
+        if (comparator != null) {
             // TODO: 02.07.16 compare with comparator
-        } else if(e != null) {
-            Comparable<? super E> eComparable = (Comparable<E>) e;
+        } else if (e != null) {
+
+            Comparable<? super E> eComparable = (Comparable<? super E>) e;
 
             Node<E> iter = root;
+            Node<E> parent = root;
+            int compareResult;
 
-            int compareResult = eComparable.compareTo(root.value);
 
-            if(compareResult > 0){
-                iter = iter.rightChild;
-            } else if(compareResult < 0){
-                iter = iter.leftChild;
+            do {
+                parent = iter;
+                compareResult = eComparable.compareTo(iter.value);
+
+                if (compareResult > 0) {
+                    iter = iter.rightChild;
+                } else if (compareResult < 0) {
+                    iter = iter.leftChild;
+                } else {
+                    return false;
+                }
+            } while (iter != null);
+            if (compareResult > 0) {
+                parent.rightChild = new Node<E>(e, parent);
             } else {
-                return false;
+                parent.leftChild = new Node<E>(e, parent);
             }
-
+            size++;
+            return true;
 
         }
         return false;
     }
 
+    @SuppressWarnings("unchecked")
+    private int canCompare(E element1, E element2) {
+
+        return comparator != null ? comparator.compare(element1, element2) : ((Comparable<? super E>) element1).compareTo(element2);
+
+    }
+
     @Override
     public boolean remove(Object o) {
-        return false;
+        Node<E> current = findNode(o);
+        if (current == null) return false;
+        size--;
+
+        if (current.rightChild == null && current.rightChild == null) {
+            if (current == root) root = null;
+            else if (current == current.parent.leftChild) current.parent.leftChild = null;
+            else current.parent.leftChild = null;
+
+            current.parent = null;
+
+        } else if (current.rightChild == null) { //has only left child
+            if (current == root) {
+                root = current.leftChild;
+                root.parent = null;
+            } else if (current == current.parent.leftChild) {
+                current.parent.leftChild = current.leftChild;
+                current.leftChild.parent = current.parent;
+                current.parent = null;
+
+            } else {
+                current.parent.rightChild = current.leftChild;
+                current.leftChild.parent = current.parent;
+                current.parent = null;
+            }
+
+        } else if (current.leftChild == null) { // has only righr child
+            if (current == root) {
+                root = current.rightChild;
+                root.parent = null;
+            } else if (current == current.parent.leftChild) {
+                current.parent.leftChild = current.rightChild;
+                current.rightChild.parent = current.parent;
+                current.parent = null;
+
+            } else {
+                current.parent.rightChild = current.leftChild;
+                current.rightChild.parent = current.parent;
+                current.parent = null;
+            }
+        } else { //has both children
+            Node<E> successor = findSuccessor(current);
+            if (current == root) {
+                root = successor;
+                root.leftChild.parent = successor;
+            } else if (current.parent.leftChild == current) {
+                current.parent.leftChild = successor;
+
+            } else {
+                current.parent.rightChild = successor;
+            }
+            successor.parent = current.parent;
+            successor.leftChild = current.leftChild;
+            current.leftChild.parent = successor;
+
+        }
+
+        return true;
+    }
+
+    private Node<E> findSuccessor(Node<E> current) {
+        Node<E> successor = current.rightChild;
+        while (successor.leftChild != null) {
+            successor = successor.leftChild;
+        }
+        if (successor.parent != current) {
+            successor.parent.leftChild = successor.rightChild;
+            successor.rightChild.parent = successor.parent;
+            successor.rightChild = current.rightChild;
+            current.rightChild.parent = successor;
+        }
+        return successor;
     }
 
     @Override
@@ -116,6 +330,11 @@ public class BinarySearchTree<E> implements Set<E>, Iterable<E> {
 
         public Node(T value) {
             this.value = value;
+        }
+
+        public Node(T value, Node<T> parent) {
+            this.value = value;
+            this.parent = parent;
         }
     }
 
--- test/main/java/data_structures/TestBinarySearchTree.java
+++ test/main/java/data_structures/TestBinarySearchTree.java
@@ -1,29 +1,53 @@
-package data_structures;
+package main.java.data_structures;
 
+import main.java.data_structures.binary_search_tree.BinarySearchTree;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Set;
 
 /**
  * Created by debasser on 02.07.16.
  */
 
 public class TestBinarySearchTree {
-    private Set<String> set;
+    BinarySearchTree set = new BinarySearchTree();
 
     @Before
-    public void setUP(){
-        System.out.println("Before");
+    public void setUP() {
+        this.set = new BinarySearchTree();
+        set.add("A");
+        set.add("B");
+        set.add("C");
     }
+
+    @After
+    public void clean() {
+        set.clear();
+    }
+
     @Test
-    public void test(){}
+    public void test_size_after_first_add() {
+        set.add("a");
+        Assert.assertTrue(set.size() == 1);
+    }
 
+    @Test
+    public void test_size_after_few_add() {
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        Assert.assertTrue(set.size() == 3);
+    }
 
+    @Test
+    public void test_size_after_not_unique_add() {
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add("c");
+        Assert.assertFalse(set.size() == 3);
+    }
 
-   @After
-    public void clean(){
-       System.out.println("After");
-   }
 }
